8ffc3d9eda07ec7206f18bfbd7b24be85bc409e2
Tony Rogvall <tony@rogvall.se>
2021-12-03 12:07:42 +0100

    fd suport

--- src/alsa_playback.erl
+++ src/alsa_playback.erl
@@ -1,5 +1,6 @@
 -module(alsa_playback).
 -export([file/1, file/2]).
+-export([fd/2]).
 
 -include("../include/alsa.hrl").
 
@@ -13,74 +14,82 @@
 file(FilePath) ->
     file(FilePath, #{}).
 
-file(FilePath, Params0) ->
+file(FilePath, Params) ->
+    case file:open(FilePath, [read, raw, binary, read_ahead]) of
+        {ok, Fd} ->
+	    try fd(Fd, Params) of
+		Result -> Result
+	    after
+		file:close(Fd)
+	    end;
+	{error, Reason} ->
+	    {error, file:format_error(Reason)}
+    end.
+
+%% Play samples from file use [ram] option to send non-file samples...
+fd(Fd, Params0) ->
     PeriodSizeInFrames = 
 	maps:get(period_size, Params0, ?PERIOD_SIZE_IN_FRAMES),
     NumBufferPeriods =
 	maps:get(buffer_periods, Params0, ?BUFFER_PERIODS),
     BufferSizeInFrames = PeriodSizeInFrames * NumBufferPeriods,
-    case file:open(FilePath, [read, raw, binary, read_ahead]) of
-        {ok, Fd} ->
-	    Params = case alsa_wav:read_header(Fd) of
-			 {ok, Header} ->
-			     io:format("wav header: ~w\n", [Header]),
-			     maps:merge(Params0, Header);
-			 _ -> 
-			     file:position(Fd, 0),
-			     Params0
-		     end,
-	    Format = maps:get(format, Params, ?DEFAULT_FORMAT),
-	    Channels0 = maps:get(channels, Params, ?DEFAULT_CHANNELS),
-	    Channels = if Channels0 =:= 1 -> 2;
-			  true -> Channels0
-		       end,
-	    Rate = maps:get(rate, Params, ?DEFAULT_RATE_IN_HZ),
-	    Device = maps:get(device, Params, ?DEFAULT_DEVICE),
-	    Pan = maps:get(pan, Params, 0.0), %% pan 0.0=left .. 1.0 = right
-            WantedHwParams =
-                #{format => Format,
-                  channels => Channels,
-                  rate => Rate,
-                  period_size => PeriodSizeInFrames,
-                  buffer_size => BufferSizeInFrames
-		 },
-            WantedSwParams =
-                #{start_threshold =>
-		      PeriodSizeInFrames * (NumBufferPeriods-1)},
-            case alsa:open(Device, playback, WantedHwParams,
-                           WantedSwParams) of
-                {ok, Handle, ActualHwParams, ActualSwParams} ->
-                    io:format("Params: ~p\n", [{ActualHwParams, ActualSwParams}]),
-		    Format1 = maps:get(format, ActualHwParams),
-		    PeriodSizeInFrames1 = maps:get(period_size, ActualHwParams),
-		    Channels1 = maps:get(channels, ActualHwParams),
-		    Size1 = alsa:format_size(Format1, Channels1),
-		    PeriodSizeInBytes = PeriodSizeInFrames1*Size1,
-		    Transform =
-			if Channels0 =:= 1, Channels1 =:= 2 ->
-				io:format("mono_to_stereo pan=~p\n", [Pan]),
-				fun (X) -> mono_to_stereo(s16_le, X, Pan) end;
-			   Channels0 =:= 2, Channels1 =:= 1 ->
-				fun (X) -> stereo_to_mono(s16_le, X) end;
-			   true ->
-				fun (X) -> X end
-			end,
-		    %% generate silence period for all frames
-		    Silence = alsa:make_silence(Format1, Channels1, 
-						PeriodSizeInFrames1),
-		    %% start by fill with silence
-		    lists:foreach(
-		      fun(_) ->
-			      alsa:write(Handle, Silence)
-		      end, lists:seq(1, NumBufferPeriods-1)),
-		    
-                    playback(Handle, Fd, PeriodSizeInBytes, Transform);
-                {error, Reason} ->
-                    file:close(Fd),
-                    {error, alsa:strerror(Reason)}
-            end;
-        {error, Reason} ->
-            {error, file:format_error(Reason)}
+    Params = case alsa_wav:read_header(Fd) of
+		 {ok, Header} ->
+		     io:format("wav header: ~w\n", [Header]),
+		     maps:merge(Params0, Header);
+		 _ -> 
+		     %% probably raw file
+		     file:position(Fd, 0),
+		     Params0
+	     end,
+    Format = maps:get(format, Params, ?DEFAULT_FORMAT),
+    Channels0 = maps:get(channels, Params, ?DEFAULT_CHANNELS),
+    Channels = if Channels0 =:= 1 -> 2;
+		  true -> Channels0
+	       end,
+    Rate = maps:get(rate, Params, ?DEFAULT_RATE_IN_HZ),
+    Device = maps:get(device, Params, ?DEFAULT_DEVICE),
+    Pan = maps:get(pan, Params, 0.0), %% pan 0.0=left .. 1.0 = right
+    WantedHwParams =
+	#{format => Format,
+	  channels => Channels,
+	  rate => Rate,
+	  period_size => PeriodSizeInFrames,
+	  buffer_size => BufferSizeInFrames
+	 },
+    WantedSwParams =
+	#{start_threshold =>
+	      PeriodSizeInFrames * (NumBufferPeriods-1)},
+    case alsa:open(Device, playback, WantedHwParams,
+		   WantedSwParams) of
+	{ok, Handle, ActualHwParams, ActualSwParams} ->
+	    io:format("Params: ~p\n", [{ActualHwParams, ActualSwParams}]),
+	    Format1 = maps:get(format, ActualHwParams),
+	    PeriodSizeInFrames1 = maps:get(period_size, ActualHwParams),
+	    Channels1 = maps:get(channels, ActualHwParams),
+	    Size1 = alsa:format_size(Format1, Channels1),
+	    PeriodSizeInBytes = PeriodSizeInFrames1*Size1,
+	    Transform =
+		if Channels0 =:= 1, Channels1 =:= 2 ->
+			io:format("mono_to_stereo pan=~p\n", [Pan]),
+			fun (X) -> mono_to_stereo(s16_le, X, Pan) end;
+		   Channels0 =:= 2, Channels1 =:= 1 ->
+			fun (X) -> stereo_to_mono(s16_le, X) end;
+		   true ->
+			fun (X) -> X end
+		end,
+	    %% generate silence period for all frames
+	    Silence = alsa:make_silence(Format1, Channels1, 
+					PeriodSizeInFrames1),
+	    %% start by fill with silence
+	    lists:foreach(
+	      fun(_) ->
+		      alsa:write(Handle, Silence)
+	      end, lists:seq(1, NumBufferPeriods-1)),
+	    
+	    playback(Handle, Fd, PeriodSizeInBytes, Transform);
+	{error, Reason} ->
+	    {error, alsa:strerror(Reason)}
     end.
 
 playback(Handle, Fd, PeriodSizeInBytes, Transform) ->
@@ -99,20 +108,18 @@ playback(Handle, Fd, PeriodSizeInBytes, Transform) ->
                     playback(Handle, Fd, PeriodSizeInBytes, Transform);
                 {error, Reason} ->
 		    alsa:close(Handle),
-                    file:close(Fd),
                     {error, alsa:strerror(Reason)}
             end;
         eof ->
 	    alsa:drain(Handle),
 	    alsa:close(Handle),
-            file:close(Fd),
             eof;
         {error, Reason} ->
 	    alsa:close(Handle),
-            file:close(Fd),
             {error, alsa:strerror(Reason)}
     end.
 
+%% Add more stuff? or use mixer device?
 mono_to_stereo(s16_le, Bin,Pan) ->
     << <<(trunc(X*(1.0-Pan))):16/signed-little, 
 	 (trunc(X*Pan)):16/signed-little>> ||
